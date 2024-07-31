8de4510a0302b6ee20fa6e9db4e77b9eddf8e077
AurelDwiBintari <aurel_bintari_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 15:03:33 +0700

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,9 @@
+public class tes {
+    public static void main (String [] args) {
+        System.out.println("Nama : Aurel Dwi");
+        System.out.println("Alamat : Pitrang Rt 1/RW 5 NO.12 kalipare.Malang jawa timur.");
+        System.out.println("SMP NEGERI 4 WARU");
+        System.out.println("No Hp :0813-2660-7125");
+    
+    }
+}
